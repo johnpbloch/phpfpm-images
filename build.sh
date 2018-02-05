9c20741bc47393bfe8ee791de4e8d551da2dca13
John P Bloch <johnpbloch@gmail.com>
2018-02-05 09:54:55 -0600

    Add PHP 7.2

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ do
   docker build --pull --no-cache -t "johnpbloch/phpfpm:$version" "images/$version"
 done
 
-docker tag johnpbloch/phpfpm:7.1 johnpbloch/phpfpm:latest
+docker tag johnpbloch/phpfpm:7.2 johnpbloch/phpfpm:latest
 
 docker push johnpbloch/phpfpm
 
